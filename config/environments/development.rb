07419fbc97057ad37641a81e44af019204fd076f
Rafael Ramirez <mrsblast@gmail.com>
2021-08-12 16:59:18 -0400

    config mailer sender

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   #Mailer configuration
-  config.action_mailer.default_url_options = { :host => "correo.cryptostore.com.bo" }
+  config.action_mailer.default_url_options = { :host => "https://foro.cryptostore.com.bo" }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
