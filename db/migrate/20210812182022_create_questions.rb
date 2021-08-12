a457312b254d00f88fbe7d68a700526fdebdbe3e
sass3r <sass3r@cryptostore.com.bo>
2021-08-12 14:48:31 -0400

    questions dashboard

---
+++ db/migrate/20210812182022_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :questions do |t|
+      t.string :title
+      t.text :description
+      t.string :user_id
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
